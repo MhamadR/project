c6b9c198c362965829f3b95e4c84d187de868e16
MhamadR <mhamadraslan@gmail.com>
2024-04-11 13:53:40 +0300

    feat: update planets model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,40 @@
-const planets = [];
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const path = require("path");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => reject(err))
+      .on("end", () => resolve());
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
