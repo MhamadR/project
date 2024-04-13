3a3d511877e05ff8d61e340b92b95e9877fabe49
MhamadR <mhamadraslan@gmail.com>
2024-04-13 17:50:07 +0300

    feat: add function to add a new launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Exploration Crew X",
   rocket: "Alpha B",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
-  customer: ["Neil, Vick"],
+  target: "Kepler-442 b",
+  customer: ["Nasa", "Explorer"],
   upcoming: true,
   success: true,
 };
@@ -17,7 +19,21 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ["Nasa", "Explorer"],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
 module.exports = {
   launches,
   getAllLaunches,
+  addNewLaunch,
 };
