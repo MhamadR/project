817dd309aa445a8db2ee3e6a41758f7dd37bb23e
MhamadR <mhamadraslan@gmail.com>
2024-04-20 22:14:50 +0300

    refactor: update app to use the versioning api router

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const api = require("./routes/api");
 
 const app = express();
 
@@ -25,8 +24,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", api);
 
 app.get("/*", (_, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
